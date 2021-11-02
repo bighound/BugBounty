6ffa47d00172b38818adcd4048f73c3a6d328000
Bighound <bighound95@hotmail.com>
2021-11-02 15:52:33 +0100

    PoC of a Bug Bounty program

--- pocDownloader.py
+++ pocDownloader.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
 
+import requests
+import time
+import sys
+
+url="https://URL={}".format(sys.argv[1])
+
+for i in range (1,500,1):
+    r = requests.get(url)
+    print(r.text)
+    if r.status_code == 200:
+        print("[+] Done. Request #{}".format(i))
+    else:
+        print("[-] Houston, we've had a problem here -__-") 
+    time.sleep(2)
